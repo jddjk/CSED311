bd5e9f99527b50fd35cca1efcf1098080220cb98
Jin7480 <jinskim7480@postech.ac.kr>
2024-03-19 18:20:33 +0900

    immediate_generator implemented

--- immediate_generator.v
+++ immediate_generator.v
@@ -7,10 +7,39 @@ module immediate_generator(instruction, imm_gen_out);
     wire [6:0] opcode;
     assign opcode = instruction[6:0];
 
-    if(opcode == `ARITHMETIC) begin
-    d;
-    
-    end else if() 
+    wire [11:0] immediate; //I-type immediate
+    assign immediate=instruction[31:20];
 
+    wire [11:0] imm; //S-type immediate
+    assign imm= {instruction[31:25], instruction[11:7]};
+   
+
+
+    always @(*) begin
+
+
+        if(opcode == `ARITHMETIC_IMM) begin// I-type instruction opcodes
+            imm_gen_out = { 20{instruction[31]},immediate};  //sign-extend(immediate)
+        
+        end else if(opcode == `LOAD) begin  // I-type instruction opcodes
+            imm_gen_out = { 20{instruction[31]},immediate};  //sign-extend(immediate)
+
+        end else if(opcode == `JALR) begin  // I-type instruction opcodes
+            imm_gen_out = { 20{instruction[31]},immediate};  //sign-extend(immediate)
+
+        end else if(opcode == `STORE) begin  // S-type instruction opcodes
+            imm_gen_out = { 20{instruction[31]},imm};  //sign-extend(imm)
+
+        end else if(opcode == `BRANCH) begin  // B-type instruction opcodes
+            imm_gen_out = { 19{instruction[31]}, immediate[31], immediate[7], immediate[30:25], immediate[11:8], 0};  //sign-extend(31, 7, 30-25, 11-8) SBtype
+
+        end else if(opcode == `JAL) begin  // J-type instruction opcodes
+            imm_gen_out = { 12{instruction[31]}, immediate[31], immediate[19:12], immediate[20], immediate[30:21], 0};  //sign-extend(31, 19-12, 20, 30-21)  UJ-type
+
+        end else begin //prevents letch error    
+            imm_gen_out = instruction;
+        end    
+
+    end
 
 endmodule 
