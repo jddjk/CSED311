af9e4474f0bb02787998ce17a65befce6c8d14b2
Jin7480 <jinskim7480@postech.ac.kr>
2024-03-19 16:53:07 +0900

    immediate_generator is implementing

--- immediate_generator.v
+++ immediate_generator.v
@@ -1,6 +1,16 @@
 `include "opcodes.v"
 
 module immediate_generator(instruction, imm_gen_out);
-intput [31:0] instruction;
-output reg [31:0] imm_gen_out;
+    input [31:0] instruction;   //followed the size written in the lecture note
+    output reg [31:0] imm_gen_out;
+
+    wire [6:0] opcode;
+    assign opcode = instruction[6:0];
+
+    if(opcode == `ARITHMETIC) begin
+    d;
+    
+    end else if() 
+
+
 endmodule 
