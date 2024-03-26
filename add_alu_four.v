a2efadaef240933f058c91e957aa0b9791063d31
Jaehyeon Lee <tps6595@postech.ac.kr>
2024-03-26 20:13:25 +0900

    Delete add_alu_four.v

--- add_alu_four.v
+++
@@ -1,9 +0,0 @@
-module add_alu_four #(parameter data_width = 32)(
-    input [data_width - 1 : 0] alu_in1,
-    output [data_width - 1 : 0] alu_result
-);
- 
-    assign alu_result = alu_in1 + 32'b100;
-
-
-endmodule 
