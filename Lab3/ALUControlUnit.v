3e5fab0d341aedfbaff332063893ed8cd8ca4221
Jin7480 <jinskim7480@postech.ac.kr>
2024-04-12 21:51:04 +0900

    alu_op added

--- Lab3/ALUControlUnit.v
+++ Lab3/ALUControlUnit.v
@@ -7,18 +7,27 @@ module ALUControlUnit (
     output reg [3:0] alu_op // by alu_control_unit
 );
 
-// alu_ctrl_op = 2'b00 -> ALU performs an add operation (l/s inst.)
-// alu_ctrl_op = 2'b01 -> ALU performs a subtract operation(branch inst.)
-// alu_ctrl_op = 2'b10 -> funct field determines the ALU operation(r-type inst.)
+    // alu_ctrl_op = 2'b00 -> ALU performs an add operation (LW,SW, inst.)
+    // alu_ctrl_op = 2'b01 -> ALU performs a subtract operation(branch inst.)
+    // alu_ctrl_op = 2'b10 -> funct field determines the ALU operation(r-type, i-type inst.)
+    // alu_ctrl_op = 2'b11 -> JAL,JALR type instruction
 
+    /* 
+    alu_op
+        00 : LW, STORE, nextPC
+        01 : R/I type
+        10 : JAL, JALR type
+        11 : Bxx type
+    */
     always @(*) begin
         case (alu_ctrl_op)
             2'b00: begin // l/s inst.
                 alu_op = `FUNC_ADD; // ADD
             end
+
             2'b01: begin // branch inst.
-                case(instruction[14:12]) 
-                    `FUNCT3_BGE: begin
+                case(instruction[14:12])  //case(funct3)
+                    `FUNCT3_BGE: begin  
                         alu_op = `FUNC_BGE; // BGE
                     end
                     `FUNCT3_BLT: begin
@@ -36,14 +45,22 @@ module ALUControlUnit (
                 endcase
             end
             2'b10: begin // r-type inst.
-                case (instruction[6:0])
+                case (instruction[6:0])   //case(opcode)
                     `ARITHMETIC: begin
-                        case (instruction[31:25])
+                        case (instruction[31:25])   //case(funct7)
                             `FUNCT7_SUB: begin
-                                alu_op = `FUNC_SUB; // SUB
+                                if(instruction[14:12]==`FUNCT3_SUB) begin
+                                    alu_op = `FUNC_SUB; // SUB
+                                end else if(instruction[14:12]==`FUNCT3_SRL) begin
+                                    alu_op = `FUNC_SRL; // Shift Right Logical
+                                end else begin
+                                    alu_op = `FUNC_ADD; // prevent letch error
+                                end 
+
                             end
+                        
                             default: begin
-                                case (instruction[14:12])
+                                case (instruction[14:12])  //case(funct3)
                                     `FUNCT3_SLL: begin
                                         alu_op = `FUNC_SLL; // SLL
                                     end
@@ -66,20 +83,27 @@ module ALUControlUnit (
                             end
                         endcase
                     end
-                    `JALR: begin
-                        alu_op = `FUNC_JALR; // JALR
-                    end
                     default: begin
                         alu_op = `FUNC_ADD; // NOP
                     end
                 endcase
             end
+
+            2'b11: begin //
+                if (instruction[6:0] == `JALR) begin //opcode == JALR
+                    alu_op = `FUNC_JALR; // JALR
+                end else begin
+                    alu_op = `FUNC_ADD; // JAL
+                end 
+                                    
+            end
+
             default: begin
                 alu_op = 4'b0000; // NOP
             end
         endcase
     end
 
+    
 
-
-endmodule 
+endmodule
